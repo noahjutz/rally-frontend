0a41227bcc18b33e51e826d5185a9073fcc8ba7a
Noah Jutz <noahjutz@tutanota.de>
2023-06-20 12:52:51 +0200

    Install bulma

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,16 +2,14 @@
 	export let data: any;
 </script>
 
-<style lang="scss">
-	:global(body, html) {
-		background-color: rgb(206, 208, 249);
-		height: 1vh;
-	}
-</style>
-
-<h1 class="title">rally-frontend</h1>
+<h1 class="title has-text-white">rally-frontend</h1>
 
 <p>This website connects to the <a href="https://api.rally.noahjutz.com">rally-api.</a></p>
 
 <p>GET / returns: {JSON.stringify(data.hellos)}</p>
 
+<style lang="scss">
+	.title::after {
+		content: ' (hi from sass!)';
+	}
+</style>
