c4f239673310bd86990dcc4ee71890a04ee4a83a
Noah Jutz <noahjutz@tutanota.de>
2023-06-23 16:46:38 +0200

    Add /signup form

--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -1,5 +1,31 @@
-<script>
-	import MyComponent from './MyComponent.svelte';
+<script lang="ts">
+	import Navbar from '$lib/components/Navbar.svelte';
 </script>
 
-<MyComponent />
+<Navbar />
+
+<section class="hero is-fullheight-with-navbar">
+	<div class="hero-body">
+		<div class="container is-max-desktop">
+			<h1 class="title">Welcome to rally.</h1>
+
+			<form>
+				<div class="field">
+					<p class="control">
+						<input class="input is-medium" type="text" placeholder="Username" />
+					</p>
+				</div>
+				<div class="field">
+					<p class="control">
+						<input class="input is-medium" type="password" placeholder="Password" />
+					</p>
+				</div>
+				<div class="field">
+					<p class="control">
+						<button class="button is-primary is-large">Sign up</button>
+					</p>
+				</div>
+			</form>
+		</div>
+	</div>
+</section>
