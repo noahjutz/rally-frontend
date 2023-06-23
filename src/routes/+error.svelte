c3c44e1f954459efbdf569af98163750968bde6a
Noah Jutz <noahjutz@tutanota.de>
2023-06-23 17:02:21 +0200

    Add +error.svelte

---
+++ src/routes/+error.svelte
@@ -0,0 +1,9 @@
+<section class="hero is-danger is-fullheight">
+	<div class="hero-body">
+		<div class="container">
+			<p class="title">That wasn't supposed to happen.</p>
+      <p class="subtitle">There was an error on our end - sorry about that.</p>
+      <a class="button is-white" href="/">Go back</a>
+		</div>
+	</div>
+</section>
