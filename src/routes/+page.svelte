2ad1ea4923cc950426529ba2c0586907063afdf2
Noah Jutz <noahjutz@tutanota.de>
2023-06-23 12:43:55 +0200

    Implement basic landing page with bulma

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,53 @@
 <script lang="ts">
 	export let data: any;
+
+	let expanded = false;
+	const toggle = () => (expanded = !expanded);
 </script>
 
-<style lang="scss">
-	:global(body, html) {
-		background-color: rgb(206, 208, 249);
-		height: 100%;
-	}
-</style>
+<section class="hero is-primary is-fullheight">
+	<div class="hero-head">
+		<nav class="navbar">
+			<div class="container">
+				<div class="navbar-brand">
+					<a class="navbar-item" href="/">
+						<img alt="logo" src="https://placehold.co/112x28" />
+					</a>
+					<button
+						class="navbar-burger"
+						class:is-active={expanded}
+						data-target="nav"
+						on:click={toggle}
+					>
+						<span /> <span /> <span />
+					</button>
+				</div>
 
-<h1 class="title has-text-white">rally-frontend</h1>
+				<div id="nav" class="navbar-menu" class:is-active={expanded}>
+					<div class="navbar-end">
+						<div class="navbar-item">
+							<div class="buttons">
+								<a class="button is-primary" href="/signup"> <strong>Sign up</strong> </a>
+								<a class="button is-light" href="/login"> Log in </a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</nav>
+	</div>
 
-<p>This website connects to the <a href="https://api.rally.noahjutz.com">rally-api.</a></p>
+	<div class="hero-body">
+		<div class="container has-text-centered">
+			<div>
+				<p class="title">rally-frontend</p>
 
-<p>GET / returns: {JSON.stringify(data.hellos)}</p>
+				<p class="subtitle">
+					This website connects to the <a href="https://api.rally.noahjutz.com">rally-api.</a>
+				</p>
 
+				<p>GET / returns: {JSON.stringify(data.hellos)}</p>
+			</div>
+		</div>
+	</div>
+</section>
