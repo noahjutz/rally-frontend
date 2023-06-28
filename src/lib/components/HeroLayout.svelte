7e01229bd0b74738ce16b2be508b02925f738653
Noah Jutz <noahjutz@tutanota.de>
2023-06-28 17:13:49 +0200

    Add /login page, create HeroLayout component

---
+++ src/lib/components/HeroLayout.svelte
@@ -0,0 +1,9 @@
+<section class="hero is-fullheight-with-navbar">
+	<div class="hero-body">
+		<div class="container">
+			<div class="columns">
+				<slot />
+			</div>
+		</div>
+	</div>
+</section>
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,15 +1,10 @@
 <script lang="ts">
 	import HeroLayout from '$lib/components/HeroLayout.svelte';
 	import Navbar from '$lib/components/Navbar.svelte';
-	import SignupForm from './SignupForm.svelte';
 </script>
 
 <Navbar />
 
 <HeroLayout>
-	<div class="column is-4 is-offset-4">
-		<h1 class="title is-size-1">Welcome to rally.</h1>
-
-		<SignupForm />
-	</div>
+	<p>todo login</p>
 </HeroLayout>
