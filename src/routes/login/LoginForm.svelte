3abb0e48c07af4d7748a15d31ec6a1545043f0b0
Noah Jutz <noahjutz@tutanota.de>
2023-06-28 17:25:36 +0200

    Clean up LoginForm.svelte script

--- src/routes/login/LoginForm.svelte
+++ src/routes/login/LoginForm.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
 
-	const submit = async (e: SubmitEvent) => {
-		const data = new FormData(e.target as HTMLFormElement);
-		const username = data.get('username');
-		const password = data.get('password');
+	let username: String;
+	let password: String;
 
+	const submit = async (e: SubmitEvent) => {
 		// todo login with api
 
 		goto('/dashboard');
@@ -15,12 +14,24 @@
 <form on:submit|preventDefault={submit}>
 	<div class="field">
 		<p class="control">
-			<input class="input is-medium" name="username" type="text" placeholder="Username" />
+			<input
+				class="input is-medium"
+				name="username"
+				type="text"
+				bind:value={username}
+				placeholder="Username"
+			/>
 		</p>
 	</div>
 	<div class="field">
 		<p class="control">
-			<input class="input is-medium" name="password" type="password" placeholder="Password" />
+			<input
+				class="input is-medium"
+				name="password"
+				type="password"
+				bind:value={password}
+				placeholder="Password"
+			/>
 		</p>
 	</div>
 	<div class="field">
