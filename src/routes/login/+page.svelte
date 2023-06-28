62c2d48d8155f8c0388cf13adfee4bfac1bb0862
Noah Jutz <noahjutz@tutanota.de>
2023-06-28 17:20:16 +0200

    Implement layout for /login

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
 	import HeroLayout from '$lib/components/HeroLayout.svelte';
 	import Navbar from '$lib/components/Navbar.svelte';
+	import LoginForm from './LoginForm.svelte';
 </script>
 
 <Navbar />
 
 <HeroLayout>
-	<p>todo login</p>
+	<div class="column is-4 is-offset-4">
+		<h1 class="title is-size-1">Welcome back.</h1>
+
+		<LoginForm />
+	</div>
 </HeroLayout>
---
+++ src/routes/login/LoginForm.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+
+	const submit = async (e: SubmitEvent) => {
+		const data = new FormData(e.target as HTMLFormElement);
+		const username = data.get('username');
+		const password = data.get('password');
+
+		// todo login with api
+
+		goto('/dashboard');
+	};
+</script>
+
+<form on:submit|preventDefault={submit}>
+	<div class="field">
+		<p class="control">
+			<input class="input is-medium" name="username" type="text" placeholder="Username" />
+		</p>
+	</div>
+	<div class="field">
+		<p class="control">
+			<input class="input is-medium" name="password" type="password" placeholder="Password" />
+		</p>
+	</div>
+	<div class="field">
+		<p class="control">
+			<button class="button is-primary is-large">Log in</button>
+		</p>
+	</div>
+</form>
