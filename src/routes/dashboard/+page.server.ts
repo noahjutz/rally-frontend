c2455e6ee70fa2f425ace49fbd48727bbe0fe09b
Noah Jutz <noahjutz@tutanota.de>
2023-07-02 17:01:59 +0200

    limit /dashboard access to logged in users

--- src/routes/dashboard/+page.server.ts
+++ src/routes/dashboard/+page.server.ts
@@ -1,4 +1,9 @@
+import { redirect } from '@sveltejs/kit';
+
 export function load({ cookies }) {
 	const token = cookies.get('token');
+
+	if (!token) throw redirect(302, '/');
+
 	return { token };
 }
