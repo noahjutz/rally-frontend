48da787bdd169621f668ba5717d26c24f2ff6229
Noah Jutz <noahjutz@tutanota.de>
2023-07-02 16:57:16 +0200

    Store JWT in cookie, move form logic to server

---
+++ src/routes/login/+page.server.ts
@@ -0,0 +1,25 @@
+import { redirect } from '@sveltejs/kit';
+
+export const actions = {
+	login: async ({ cookies, request }) => {
+		const api = import.meta.env.VITE_RALLY_API_URL;
+
+		const data = await request.formData();
+		const user = {
+			username: data.get('username'),
+			password: data.get('password')
+		};
+
+		const tokenResponse = await fetch(api + '/login', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(user)
+		});
+		const tokenJson = await tokenResponse.json();
+		const token = tokenJson['token'];
+
+		cookies.set('token', token);
+
+		throw redirect(307, '/dashboard');
+	}
+};
--- src/routes/login/LoginForm.svelte
+++ src/routes/login/LoginForm.svelte
@@ -1,43 +1,12 @@
-<script lang="ts">
-	import { goto } from '$app/navigation';
-
-	const api = import.meta.env.VITE_RALLY_API_URL;
-
-	let username: String;
-	let password: String;
-
-	const submit = async (e: SubmitEvent) => {
-		await fetch(api + '/login', {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify({ username, password })
-		});
-
-		goto('/dashboard');
-	};
-</script>
-
-<form on:submit|preventDefault={submit}>
+<form action="?/login" method="POST">
 	<div class="field">
 		<p class="control">
-			<input
-				class="input is-medium"
-				name="username"
-				type="text"
-				bind:value={username}
-				placeholder="Username"
-			/>
+			<input class="input is-medium" name="username" type="text" placeholder="Username" />
 		</p>
 	</div>
 	<div class="field">
 		<p class="control">
-			<input
-				class="input is-medium"
-				name="password"
-				type="password"
-				bind:value={password}
-				placeholder="Password"
-			/>
+			<input class="input is-medium" name="password" type="password" placeholder="Password" />
 		</p>
 	</div>
 	<div class="field">
